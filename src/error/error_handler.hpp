3e86057833a0bb053e45d222a70bf5da8c2f6ece
Nacho Valladares <nacho.valladares@pknoa.com>
2017-06-05 08:26:01 +0200

    Init Cmake project. Dummy errors class included

---
+++ src/error/error_handler.hpp
@@ -0,0 +1,16 @@
+#ifndef SKYLINES_ERROR_HANDLER_HPP
+#define SKYLINES_ERROR_HANDLER_HPP
+
+#include "export_import.hpp"
+
+namespace skylines { namespace error {
+    class skylines_DLL_EXPORTS ErrorHandler {
+    public:
+        ErrorHandler() {}
+
+
+    private:
+    };
+}}
+
+#endif // SKYLINES_ERROR_HANDLER
